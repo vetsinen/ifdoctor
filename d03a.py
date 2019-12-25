ad2d28fc0d05ec1ca136702b3e0fbc7103064de1
vetsinen <vetsinen@gmail.com>
2019-12-25 15:49:51 +0200

    initial message

---
+++ d03a.py
@@ -0,0 +1,11 @@
+#результаты проверки можно сохранять в переменные
+
+c = 5 < 4
+print(c)
+
+p = "python" > "java"
+print(p)
+if p:
+    print("python is the best")
+
+print(type(p)) #переменная будет иметь так называемый boolean-тип
